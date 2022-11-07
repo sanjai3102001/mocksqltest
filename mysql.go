7a02f10642ac49c7e61c93428b4ad5b2c7c62ce6
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2022-11-07 17:07:08 +0530

    modified the if condition

--- mysql.go
+++ mysql.go
@@ -60,16 +60,16 @@ func (r *repository) Find() ([]*repo.UserModel, error) {
 	defer cancel()
 
 	// rows, err := r.db.QueryContext(ctx, "SELECT id, name, email, phone FROM users")
-	rows, err := r.db.QueryContext(ctx, "SELECT id, name, email, phone FROM users")
-	if err != nil {
+	rows, _ := r.db.QueryContext(ctx, "SELECT id, name, email, phone FROM users")
+	if rows == nil {
 		// return nil, err
-		fmt.Println("got an error while getting the rows", err)
+		fmt.Println("got an error while getting the rows")
 	}
 	defer rows.Close()
 
 	for rows.Next() {
 		user := new(repo.UserModel)
-		err = rows.Scan(
+		err := rows.Scan(
 			&user.ID,
 			&user.Name,
 			&user.Email,
